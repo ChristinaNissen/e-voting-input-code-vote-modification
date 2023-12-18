70586a3c33a5d416faafdded3839538ea6a009b0
Christina Nissen <christinaf.nissen@hotmail.com>
2023-12-18 10:19:32 +0100

    Change of info-3 + adding connectection to the new database

--- src/Info-Pages/Info-3.js
+++ src/Info-Pages/Info-3.js
@@ -21,29 +21,35 @@ import React from "react";
 import {
   Button,
   Text,
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
 } from "@chakra-ui/react";
+import getCurrentUser from "../API/Voter";
+import { CopyIcon } from "@chakra-ui/icons";
 import "./InfoPages.css";
 
 export default function Info3() {
-  /* const voter = getCurrentUser(); */
+  const voter = getCurrentUser();
 
-/*   function copyIdToClipBoard() {
+  function copyIdToClipBoard() {
     navigator.clipboard.writeText(voter.attributes.username);
-  } */
+  }
   return (
     <div className="container-info-pages">
       <div className="inner-box-info centered-info-page">
         <h2 className="h2-info-pages">Please tell us about your experience</h2>
-        <Text className="medium-body-text-info">
+       {/*  <Text className="medium-body-text-info">
           Congratulations! You have finished the second part of the study.
-        </Text>
+        </Text> */}
 
         <Text className="medium-body-text-info">
           To complete the study, please fill out a survey about your experience
           of the online voting system.
         </Text>
 
-{/*         <Text className="medium-body-text-info">
+        <Text className="medium-body-text-info">
           We need to be able to connect your results from the voting system with
           the survey. Therefore, you have to copy the number just below and
           paste into survey as the very first thing, after you click the button
@@ -59,19 +65,17 @@ export default function Info3() {
           <InputRightElement width="4.5rem">
             <IconButton
               className="copy-button"
-              color="white"
               size="sm"
               onClick={copyIdToClipBoard}
               icon={<CopyIcon />}
             />
           </InputRightElement>
-        </InputGroup> */}
+        </InputGroup>
 
         <Button
           marginTop={"2rem"}
           width="8rem"
           className="red-btn"
-          color="white"
           onClick={() =>
             (window.location.href =
               "https://www.survey-xact.dk/LinkCollector?key=C7LXSJLQLP9J&System=1.0")
